fd6ae24779cf33c7e68be7d819821fc4eb6592f9
Sir.DNT <sir.dnt@gmail.com>
2021-02-11 22:52:51 +0700

    fix script push

--- docker_hub_push.sh
+++ docker_hub_push.sh
@@ -2,10 +2,10 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   
 # build production docker images
-docker build -t sirdnt/fc-client ./client/Dockerfile ./client
-docker build -t sirdnt/fc-api ./server/Dockerfile ./server
-docker build -t sirdnt/fc-nginx ./nginx/Dockerfile ./nginx
-docker build -t sirdnt/fc-worker ./worker/Dockerfile ./worker
+docker build -t sirdnt/fc-client ./client
+docker build -t sirdnt/fc-api ./server
+docker build -t sirdnt/fc-nginx ./nginx
+docker build -t sirdnt/fc-worker ./worker
 
 # push image to docker hub
 docker push sirdnt/fc-client
